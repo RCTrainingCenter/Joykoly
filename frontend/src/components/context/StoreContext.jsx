22e9e41360a9879e812790ef8a8b32da8af3c649
RCTrainingCenter <104927920+RCTrainingCenter@users.noreply.github.com>
2025-06-15 00:25:03 +0600

    Update StoreContext.jsx

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext=createContext(null);
 const StoreContextProvider=(props)=>{
 const [cartItems,setCartItems]=useState({});
 
-const url = "https://joykoly-publications.onrender.com";
+const url = "https://joykoly-api.onrender.com";
 // const url = 'http://localhost:4000'
 const [token,setToken]= useState("");
 const [food_list,setFoodList] = useState([]);
